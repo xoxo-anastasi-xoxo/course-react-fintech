e64f804d762de26a9f5bf4277269cc2afd880f98
Anastasia <nastya.k.r@mail.ru>
2017-11-10 17:37:55 +0300

    with travis

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,6 @@
 import React from 'react';
 
-export default ({type, onClick, children}) => <button type={type} onClick={onClick}>{children}</button>
+export default ({type, onClick, children}) =>
+  <button type={type} onClick={onClick}>
+    {children}
+  </button>;
